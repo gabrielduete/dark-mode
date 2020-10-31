c48544235d900381064ed9fd6c73fe2ff9a8ee39
Gabriel Monteiro Duete <59345698+gabrielduete@users.noreply.github.com>
2020-10-31 19:28:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var mudar = false
 function mudarCor(){ // Função para mudar o tema do página ao clicar na imagem.
     if (mudar == false){
-        document.body.style.backgroundColor = '#333'
-        document.body.style.color = '#fff'
+        document.body.style.backgroundColor = 'rgb(0, 0, 0)'
+        document.body.style.color = 'rgb(255, 255, 255)'
+        document.getElementById('1').style.backgroundColor = 'rgb(255, 255, 255)'
+        document.getElementById('1').style.color = 'rgb(0, 0, 0)'
+        document.getElementById('2').style.backgroundColor = 'rgb(255, 255, 255)'
+        document.getElementById('2').style.color = 'rgb(0, 0, 0)'
+        document.getElementById('sol').style.display = 'inline'
+        document.getElementById('lua').style.display = 'none'
+
         mudar = true
     }else{
-        document.body.style.backgroundColor = 'rgb(228, 220, 220)'
-        document.body.style.color = '#333'
+        document.body.style.backgroundColor = 'rgb(255, 255, 255)'
+        document.body.style.color = 'rgb(0, 0, 0)'
+        document.getElementById('1').style.color = 'rgb(255, 255, 255)'
+        document.getElementById('1').style.backgroundColor = 'rgb(0, 0, 0)'
+        document.getElementById('2').style.color = 'rgb(255, 255, 255)'
+        document.getElementById('2').style.backgroundColor = 'rgb(0, 0, 0)'
+        document.getElementById('lua').style.display = 'inline'
+        document.getElementById('sol').style.display = 'none'
         mudar = false
     }
 }
